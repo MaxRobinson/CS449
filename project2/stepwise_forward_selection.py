8ab9e25572e9b262e70352fb10bc863d3b4ce044
Max Robinson <maxrgit@gmail.com>
2017-09-23 20:07:43 -0400

    Implemented SFS and have a basic test

--- project2/stepwise_forward_selection.py
+++ project2/stepwise_forward_selection.py
@@ -1,9 +1,64 @@
 """ Created by Max Robinson 9/20/2017 """
+import sys
+import copy
+from testLearner import TestLeaner
+from customCsvReader import CustomCSVReader
 
 
 class SFS:
-    pass
 
+    @staticmethod
+    def select_features(Features, data_training, data_test, learner):
+        """
+
+        :param Features: an Array of unique integers that correspond to a feature number
+        in a data point in the data_training and data_test data sets.
+        :param data_training: a list of lists where each inner list is a data point with n features.
+        :param data_test: a list of lists where each inner list is a data point with n features.
+        :param learner: A class that has the two functions learn and evaluate
+        :return: a list of which features were selected which optimized the learners score.
+        """
+        original_features = copy.deepcopy(Features)
+        selected_features = []
+        base_preference_f = -sys.maxsize
+
+        while len(Features) >= 0:
+            best_perf = -sys.maxsize
+            best_feature = None
+
+            for feature in Features:
+                selected_features.append(feature)
+                model = learner.learn(selected_features, data_training)
+                curr_perf = learner.evaluate(model, selected_features, data_test)
+
+                if curr_perf > best_perf:
+                    best_perf = curr_perf
+                    best_feature = feature
+
+                selected_features.remove(feature)
+
+            if best_perf > base_preference_f:
+                base_preference_f = best_perf
+                Features.remove(best_feature)
+                selected_features.append(best_feature)
+
+            else:
+                break
+        return selected_features
+
+
+all_data = CustomCSVReader.read_file("data/iris.data.txt", float)
+data_training = all_data[:2*int(len(all_data)/3)]
+data_test = all_data[2*int(len(all_data)/3):]
+feature_length = len(all_data[0]) - 1
+
+Features = list(range(feature_length))
+
+testLearner = TestLeaner()
+
+best_features = SFS.select_features(Features, data_training, data_test, testLearner)
+
+print(best_features)
 
 """
 Pseudo code
