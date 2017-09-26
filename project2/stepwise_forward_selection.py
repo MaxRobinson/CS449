8b987f0cf3c42f484b941289217a0fe97360fff1
Max Robinson <maxrgit@gmail.com>
2017-09-26 01:27:53 -0400

    SFS working with Kmeans and have results.

--- project2/stepwise_forward_selection.py
+++ project2/stepwise_forward_selection.py
@@ -4,6 +4,7 @@ import copy
 import pprint
 from testLearner import TestLeaner
 from k_means import KMeans
+from hierarchical_agglomerative_clustering import HAC
 from customCsvReader import CustomCSVReader
 
 
@@ -32,8 +33,8 @@ class SFS:
                 selected_features.append(feature)
                 model = learner.learn(selected_features, data_training)
                 curr_perf = learner.evaluate(model, selected_features, data_test)
-                print("Selected Features: {}".format(selected_features))
-                print("Score: {}".format(curr_perf))
+                # print("Selected Features: {}".format(selected_features))
+                # print("Score: {}".format(curr_perf))
 
 
                 if curr_perf > best_perf:
@@ -46,16 +47,17 @@ class SFS:
                 base_preference_f = best_perf
                 Features.remove(best_feature)
                 selected_features.append(best_feature)
-                print()
-                print("Setting BEST selected Features")
-                print("Best Selected Features: {}".format(selected_features))
-                print("Score: {}".format(curr_perf))
-                print()
+                # print()
+                # print("Setting BEST selected Features")
+                # print("Best Selected Features: {}".format(selected_features))
+                # print("Score: {}".format(curr_perf))
+                # print()
 
 
             else:
                 break
         return selected_features, base_preference_f
+        # return selected_features
 
 
 # all_data = CustomCSVReader.read_file("data/iris.data.txt", float)
@@ -67,43 +69,40 @@ class SFS:
 #
 # print("\nBest Features Selected: {}\n".format(best_features))
 
-def run_experiment(data_set_path, number_of_clusters, learner, data_type=float):
-    print("Running {0} Experiment with k clusters ={1}".format(data_set_path, number_of_clusters))
+def run_kmeans_experiment(data_set_path, number_of_clusters, learner, fraction_of_data_used=1, data_type=float):
+    print("Running {0} Experiment with k clusters = {1}".format(data_set_path, number_of_clusters))
     all_data = CustomCSVReader.read_file(data_set_path, data_type)
+    feature_selection_data = all_data[:int(len(all_data)/fraction_of_data_used)]
     feature_length = len(all_data[0]) - 1
 
     Features = list(range(feature_length))
-    best_features = SFS.select_features(Features, all_data, all_data, learner)
+    best_features = SFS.select_features(Features, feature_selection_data, all_data, learner)
 
-    print("The Final Selected Features are: ")
-    print("{}".format(best_features[0]))
-    print("The Fisher Score for the features is: ")
-    print("{}".format(best_features[1]))
-
-    # pp = pprint.PrettyPrinter(indent=2)
-    # print("Learned Naive Bayes Distribution: ")
-    # print("Keys are structured as follows: (feature#, possible domain values 0 or 1, 'label', label value)")
-    # print("Special Key's that are ('label', possible_class_value) are the percentage of the distribution with that class label")
-    # pp.pprint(distro_1)
-    # print()
-    #
-    # # Evaluate
-    # c2 = classify(distro_1, set_2)
-    #
-    # print("Results for Test Set: \n")
-    # for predicted_class, test_record in zip(c2, set_2):
-    #     print("Predicted Class: {}".format(predicted_class[0][0]))
-    #     print("Actual Class: {}".format(get_class(test_record)))
-    #     print("Test feature Vector (last feature is actual class): \n{} \n".format(test_record))
-    #
-    # evaluation_1 = evaluate(set_2, c2)
-    # print("Error Rate = {}".format(evaluation_1))
-    # print()
+    means = learner.learn(best_features[0], all_data)
+    data_clusters = learner.get_clusters_for_means(means, best_features[0], all_data)
 
 
-# run_experiment("data/iris.data.txt", 3, KMeans(3))
-# run_experiment("data/glass.data.txt", 6, KMeans(6))
-run_experiment("data/spambase.data.txt", 2, KMeans(2))
+    print("The Final Selected Features are: (features are zero indexed) ")
+    print("{}\n".format(best_features[0]))
+    print("The Fisher Score for the clustering is: ")
+    print("{}\n".format(best_features[1]))
+
+    pp = pprint.PrettyPrinter(indent=2, width=400)
+    print("For Clustered points, the key in the dictionary represents the cluster each data point belongs to. ")
+    print("Clustered points: ")
+    pp.pprint(data_clusters)
+
+
+
+sys.stdout = open('results/SFS-Kmeans-iris-results.txt', 'w')
+run_kmeans_experiment("data/iris.data.txt", 3, KMeans(3))
+
+sys.stdout = open('results/SFS-Kmeans-glass-results.txt', 'w')
+run_kmeans_experiment("data/glass.data.txt", 6, KMeans(6))
+
+sys.stdout = open('results/SFS-Kmeans-spambase-results.txt', 'w')
+run_kmeans_experiment("data/spambase.data.txt", 2, KMeans(2))
+
 
 """
 Pseudo code
