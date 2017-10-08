ff25098cede5d7b4e7ef270a98d922c9e34f5dff
Max Robinson <maxrgit@gmail.com>
2017-10-08 13:18:08 -0400

    Removed K search code

--- project3/knn.py
+++ project3/knn.py
@@ -1,9 +1,6 @@
 from __future__ import division
-import matplotlib.pyplot as plt
 import csv, math, random, sys
 import datetime
-import numpy as np
-import matplotlib.mlab as mlab
 
 from customCsvReader import CustomCSVReader
 
@@ -125,96 +122,6 @@ class Knn:
             else:
                 labels[classLabel] += 1
         return max(labels, key=labels.get)
-
-# <editor-fold desc="Validation Curves">
-def generate_validation_curves(x_axis_values, values_line_1, values_line_2, label_1, label_2, x_axis_label="", y_axis_label="MSE", title=""):
-    plt.plot(x_axis_values, values_line_1, '-', label=label_1)
-    plt.plot(x_axis_values, values_line_2, '-', label=label_2)
-
-    plt.xlabel(x_axis_label)
-    plt.ylabel(y_axis_label)
-    plt.title(title)
-
-    plt.legend(loc='best')
-
-    plt.show()
-    plt.close()
-
-# </editor-fold>
-
-# <editor-fold desc="Procedure">
-def get_squared_error(predicted_value, item):
-    actual_value = item[-1]
-    squared_error = (predicted_value - actual_value)**2
-    return squared_error
-
-
-def calculate_mse(k, training_data, test_data):
-    squared_error = []
-    # for item in test_data:
-    #     value = knn(k, training_data, item)
-    #     squared_error.append(get_squared_error(value, item))
-    knn = Knn(k, classification=False)
-    predictions = knn.learn_many(training_data, test_data)
-    for prediction, test_item in zip(predictions, test_data):
-        squared_error.append(get_squared_error(prediction, test_item))
-
-    squared_error_sum = sum(squared_error)
-
-    mse = squared_error_sum / len(squared_error)
-
-    return mse
-
-
-def get_best_k_for_data_mse(dataset):
-    k_list = range(1, 11)
-
-    # dataset = read_file()
-    random.shuffle(dataset)
-
-    two_thirds = 2 * int(math.floor(len(dataset)/3))
-    training_data = dataset[:two_thirds]
-    test_data = dataset[two_thirds:]
-
-    test_mse_list = []
-    train_mse_list = []
-
-    for k in k_list:
-        test_mse_list.append(calculate_mse(k, training_data, test_data))
-        train_mse_list.append(calculate_mse(k, training_data, training_data))
-
-    generate_validation_curves(k_list, train_mse_list, test_mse_list, "Training Data Mean Squared Error",
-                               "Test Data Mean Squared Error", title="Number of K's vs MSE", x_axis_label="# of k's")
-
-    min_error = min(test_mse_list)
-    return test_mse_list.index(min_error) + 1
-
-
-def learning_curves(k, dataset):
-    # dataset = read_file()
-    random.shuffle(dataset)
-
-    two_thirds = 2 * int(math.floor(len(dataset)/3))
-    training_data = dataset[:two_thirds]
-    test_data = dataset[two_thirds:]
-
-    percent_increase = range(5, 105, 5)
-
-    training_mse_list = []
-    test_mse_list = []
-
-    for percentage_of_data in percent_increase:
-        amount_of_data = int(math.floor(len(training_data) * (percentage_of_data * .01)))
-        subset_data = training_data[:amount_of_data]
-
-        test_mse_list.append(calculate_mse(k, subset_data, test_data))
-        training_mse_list.append(calculate_mse(k, subset_data, subset_data))
-
-    generate_validation_curves(percent_increase, training_mse_list, test_mse_list, "Training Data Mean Squared Error",
-                               "Test Data Mean Squared Error", title="Learning Curve for k={}".format(k),
-                               x_axis_label="Percentage of data Used")
-
-# </editor-fold>
 
 
 # <editor-fold desc="Test">
