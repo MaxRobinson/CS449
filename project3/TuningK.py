4aaa4f273db48800c7ec47463139d4e5a8d5fdc9
Max Robinson <maxrgit@gmail.com>
2017-10-08 22:25:30 -0400

    docs for tuningk

--- project3/TuningK.py
+++ project3/TuningK.py
@@ -12,8 +12,11 @@ from condensedNN import CondensedNN
 
 # <editor-fold desc="Validation Curves">
 def generate_validation_curves(x_axis_values, values_line_1, label_1, x_axis_label="", y_axis_label="MSE", title=""):
+    """
+    A helper method to create a graph of the k's and the performance of different k values with respect to their
+    error rates. Shows plot to user. User saves the plot manually.
+    """
     plt.plot(x_axis_values, values_line_1, '-', label=label_1)
-    # plt.plot(x_axis_values, values_line_2, '-', label=label_2)
 
     plt.xlabel(x_axis_label)
     plt.ylabel(y_axis_label)
@@ -28,26 +31,50 @@ def generate_validation_curves(x_axis_values, values_line_1, label_1, x_axis_lab
 
 # <editor-fold desc="Procedure">
 def get_best_k_for_data_mse(data_set_path, number_of_runs):
+    """
+    Does a search over the k space to find the best k value for regression based k-NN
+
+    Runs the cross validation experiment with a set of data n number of runs.
+    The results from the cross validation for each run of k is stored then averaged
+    over the total number of runs.
+
+    The result is then ploted
+    """
     k_list = range(1, 11)
 
     train_mse_list_master = [0] *len(k_list)
 
-    for x in range(30):
+    for x in range(number_of_runs):
         train_mse_list = []
+
+        # compute performance for each k value
         for k in k_list:
             knn = Knn(k, False)
             average_mse = Experiments.run_experiment_regression(data_set_path, k, knn)
             train_mse_list.append(average_mse)
 
+        # add values to the master list of k values to be averaged later
         for index in range(len(train_mse_list_master)):
             train_mse_list_master[index] += train_mse_list[index]
+
+    # average all of the values for each k tested by the number of times k was tested
     for index in range(len(train_mse_list_master)):
         train_mse_list_master[index] /= number_of_runs
+
     generate_validation_curves(k_list, train_mse_list_master, "Average Mean Squared Error",
                                title="Number of K's vs AMSE", x_axis_label="# of k's", y_axis_label="MSE")
 
 
 def get_best_k_for_data_error_rate_normal(data_set_path, number_of_runs):
+    """
+    Does a search over the k space to find the best k value for classification based k-NN
+
+    Runs the cross validation experiment with a set of data n number of runs.
+    The results from the cross validation for each run of k is stored then averaged
+    over the total number of runs.
+
+    The result is then ploted
+    """
     k_list = range(1, 11)
 
     train_mse_list_master = [0] * len(k_list)
@@ -68,6 +95,15 @@ def get_best_k_for_data_error_rate_normal(data_set_path, number_of_runs):
                                y_axis_label="Error Rate")
 
 def get_best_k_for_data_error_rate_condensed(data_set_path, number_of_runs):
+    """
+    Does a search over the k space to find the best k value for condensed k-NN classification.
+
+    Runs the cross validation experiment with a set of data n number of runs.
+    The results from the cross validation for each run of k is stored then averaged
+    over the total number of runs.
+
+    The result is then ploted
+    """
     k_list = range(1, 11)
 
     train_mse_list_master = [0] * len(k_list)
@@ -91,15 +127,15 @@ def get_best_k_for_data_error_rate_condensed(data_set_path, number_of_runs):
 # </editor-fold>
 
 # Regression
-# get_best_k_for_data_mse("data/machine.data.new.txt", 20)
-# get_best_k_for_data_mse("data/forestfires.data.new.txt", 20)
+get_best_k_for_data_mse("data/machine.data.new.txt", 20)
+get_best_k_for_data_mse("data/forestfires.data.new.txt", 20)
 
 # Classification
-# get_best_k_for_data_error_rate_normal("data/ecoli.data.new.txt", 20)
-# get_best_k_for_data_error_rate_normal("data/segmentation.data.new.txt", 2)
+get_best_k_for_data_error_rate_normal("data/ecoli.data.new.txt", 20)
+get_best_k_for_data_error_rate_normal("data/segmentation.data.new.txt", 5)
 
 
 # Condensed Classification
-# get_best_k_for_data_error_rate_condensed("data/ecoli.data.new.txt", 2)
-# get_best_k_for_data_error_rate_condensed("data/segmentation.data.new.txt", 2)
+get_best_k_for_data_error_rate_condensed("data/ecoli.data.new.txt", 10)
+get_best_k_for_data_error_rate_condensed("data/segmentation.data.new.txt", 1)
 
