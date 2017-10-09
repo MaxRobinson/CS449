f010c1a48f0e6dc267f5f2d8778597a3613d05f6
Max Robinson <maxrgit@gmail.com>
2017-10-08 22:47:26 -0400

    docs for CondensedNN

--- project3/condensedNN.py
+++ project3/condensedNN.py
@@ -6,9 +6,24 @@ from customCsvReader import CustomCSVReader
 
 class CondensedNN:
     def __init__(self):
+        """
+        Use k-NN with k=1 to find the closes point in Z for condensing
+        """
         self.knn = Knn(1, True)
 
     def condense(self, training_set):
+        """
+        Create the condensed model from the training set.
+
+        Start with an empty set Z.
+        While Z continues to change, repeat the following:
+            For each data point in the training set x', find a point x^''in Z that is the closest point in Z to x'.
+            If the labels of x^' and x'' are not the same, add x' to Z
+
+
+        :param training_set: data points to select the condensed model from.
+        :return: list of data points, the condensed model
+        """
         random.shuffle(training_set)
 
         z = []
@@ -27,32 +42,11 @@ class CondensedNN:
 
 
     def find_nearest_z_class(self, z, datapoint):
+        """
+        Use the k-NN code with k = 1 to find the closest point in Z to the data point in question.
+        :param z: Selected data points for condensed model
+        :param datapoint: data point to query in Z
+        :return: returns the label of the closest point
+        """
         return self.knn.learn(z, datapoint)
-
-
-# all_data = CustomCSVReader.read_file("data/ecoli.data.new.txt")
-# random.shuffle(all_data)
-# training_data = all_data[:2*(int(len(all_data)/3))]
-#
-# test_data = all_data[2*(int(len(all_data)/3)):]
-#
-#
-# cnn = CondensedNN()
-# z = cnn.condense(training_data)
-#
-# print(len(z))
-# print(z)
-# print("All Data len {}".format(len(all_data)))
-#
-# knn = Knn(4, True)
-#
-# results = knn.test(z, test_data)
-#
-# error = 0
-# for result, datapoint in zip(results, test_data):
-#     if result != datapoint[-1]:
-#         error += 1
-#
-# print(error/len(results))
-
 
